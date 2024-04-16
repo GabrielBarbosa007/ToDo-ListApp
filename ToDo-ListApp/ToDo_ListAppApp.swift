bf26e1e7d0ef415d385708367090cd5df539e248
Gabriel Barbosa <gabrielbarbos210@gmail.com>
2024-04-16 18:59:57 -0300

    Initial Commit

---
+++ ToDo-ListApp/ToDo_ListAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDo_ListAppApp.swift
+//  ToDo-ListApp
+//
+//  Created by Gabriel Barbosa on 16/04/24.
+//
+
+import SwiftUI
+
+@main
+struct ToDo_ListAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
